f76c1e2d70237ca1bb8be0c0daaeaa96dc7b59cc
Basit Waseem <abdulbasit_biztek@hotmail.com>
2020-07-11 04:13:27 -0700

    stopwatch_with_javascript v1

---
+++ app.js
@@ -0,0 +1,52 @@
+var hour = 0;
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var hourHead = document.getElementById("hour");
+var minHead = document.getElementById("min");
+var secHead = document.getElementById("sec");
+var msecHead = document.getElementById("msec");
+var interval;
+
+function timer() {
+    msec++;
+    msecHead.innerHTML = msec;
+    if(msec >= 100){
+        sec++;
+        secHead.innerHTML = sec;
+        msec = 0;
+    }else if(sec >= 60){
+        min++;
+        minHead.innerHTML = min;
+        sec = 0;
+        msec = 0;
+    }else if(min >= 60){
+        hour++;
+        hourHead.innerHTML = hour;
+        min = 0;
+        sec = 0;
+        msec = 0;
+    }
+}
+
+function start() {
+    clearInterval(interval);
+    interval = setInterval(timer, 10);
+}
+
+function stop() {
+    clearInterval(interval);
+}
+
+function reset() {
+    var hour = 0;
+    var min = 0;
+    var sec = 0;
+    var msec = 0;
+    hourHead.innerHTML = hour;
+    minHead.innerHTML = min;
+    secHead.innerHTML = sec;
+    msecHead.innerHTML = msec;
+    stop();
+}
